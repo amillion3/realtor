de6d676f2c924d6dfd1bd5477020e474c5565222
Andy <amillion3@gmail.com>
2018-06-26 19:15:04 -0500

    added ListingForm component

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
+import ListingForm from '../components/ListingForm/ListingForm';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Listings></Listings>
         <Building></Building>
+        <ListingForm></ListingForm>
       </div>
     );
   }
