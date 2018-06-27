d944bbadb9f97ff2deeaaa3308197a25dce79aa8
Andy <amillion3@gmail.com>
2018-06-26 19:05:42 -0500

    added Building component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
+import Building from '../components/Building/Building';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Listings />
+      <div className="App">
+        <Listings></Listings>
+        <Building></Building>
+      </div>
     );
   }
 }
